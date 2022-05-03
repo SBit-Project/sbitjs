e013e74dc940b5a9ad719c7a2777d45bb1264199
Scot Lynne <103759613+ScotLynne@users.noreply.github.com>
2022-05-03 17:46:53 +0430

    Update EventListener.d.ts

--- lib/EventListener.d.ts
+++ lib/EventListener.d.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from "eventemitter3";
-import { SBitRPC, IRPCWaitForLogsRequest, IPromiseCancel } from "./SBitRPC";
+import { SbitRPC, IRPCWaitForLogsRequest, IPromiseCancel } from "./SbitRPC";
 import { ContractLogDecoder } from "./abi";
 import { IContractEventLogs, IContractEventLog } from "./Contract";
 export declare type ICancelFunction = () => void;
@@ -9,7 +9,7 @@ export interface ICancellableEventEmitter extends EventEmitter {
 export declare class EventListener {
     private rpc;
     private logDecoder;
-    constructor(rpc: SBitRPC, logDecoder: ContractLogDecoder);
+    constructor(rpc: SbitRPC, logDecoder: ContractLogDecoder);
     /**
      * Get contract event logs. Long-poll wait if no log is found. Returns a cancel
      * function that stops the events subscription.
